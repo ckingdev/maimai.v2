b4186e39468f3341057cb91ad8efcf2d2cdb07c5
Cameron Palone <mcameronpalone@gmail.com>
2015-08-10 13:09:42 -0600

    Use test cfg and conform to jedevc's guidelines.

--- main/main.go
+++ main/main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"github.com/cpalone/gobot/config"
+	"github.com/cpalone/gobot/handlers"
 	"github.com/cpalone/maimai.v2"
 )
 
 
 func main() {
-	b, err := config.BotFromCfgFile("maimai.yml")
+	b, err := config.BotFromCfgFile("test.yml")
 	if err != nil {
 		panic(err)
 	}
 	for _, room := range b.Rooms {
-		room.Handlers = append(room.Handlers, &maimai.LinkTitleHandler{})
+		room.Handlers = append(room.Handlers, 
+		&maimai.LinkTitleHandler{},
+		 &handlers.UptimeHandler{}, 
+		&handlers.PongHandler{},
+		&handlers.HelpHandler{ShortDesc:"short help", LongDesc: "long help"})
 	}
 	b.RunAllRooms()
 }
