fa696cd2d7a35eb9c0699d9be17dbcd140e6dc7d
Cameron Palone <mcameronpalone@gmail.com>
2015-08-21 16:52:25 -0600

    Simplify main.

--- main/main.go
+++ main/main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"sync"
-
-	"github.com/cpalone/gobot"
 	"github.com/cpalone/gobot/config"
 	"github.com/cpalone/maimai.v2"
 )
@@ -19,15 +15,5 @@ func main() {
 			&maimai.LinkTitleHandler{},
 		)
 	}
-	fmt.Println(b.Rooms)
-	var wg sync.WaitGroup
-	for _, room := range b.Rooms {
-		wg.Add(1)
-		go func(r *gobot.Room) {
-			defer wg.Done()
-			err := r.Run()
-			fmt.Printf("Room %s finished running: %s", r.RoomName, err)
-		}(room)
-	}
-	wg.Wait()
+	b.RunAllRooms()
 }
